e041c1f88ae6b913b3bac5c195cf8ba32e76a310
Giorgi Julian Ordo?ez <est_gj_ordonez@fesc.edu.co>
2026-05-11 23:04:18 -0500

    Actualizacion y subida de ficheros Semana 7-8-9-10

---
+++ docs uni/Guias Resueltas/Arquitectura y Diseno de Software/1er Corte/Semana 2/app/Data/conexion.php
@@ -0,0 +1,4 @@
+<?php
+/* Asignatura: Arquitectura y Diseño de Software | Autor: james | Guía: 2 */
+echo "Capa de Datos conectada con éxito<br>";
+?>
---
+++ docs uni/Guias Resueltas/Arquitectura y Diseno de Software/1er Corte/Semana 2/index.php
@@ -0,0 +1,5 @@
+<?php
+/* Asignatura: Arquitectura y Diseño de Software | Autor: james | Guía: 2 */
+echo "<h1>Proyecto Iniciado</h1>";
+include 'app/Data/conexion.php';
+?>
